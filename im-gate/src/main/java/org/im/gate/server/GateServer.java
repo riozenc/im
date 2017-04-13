c2dbc9a15d83cbccbf0db3f86f47cdf55c4390cb
riozenc <riozenc@DESKTOP-US6TTP8>
2017-04-13 18:12:47 +0800

    1

---
+++ im-gate/src/main/java/org/im/gate/server/GateServer.java
@@ -0,0 +1,10 @@
+/**
+ * Title:GateServer.java
+ * Author:riozenc
+ * Datetime:2017年4月13日 下午5:04:41
+**/
+package org.im.gate.server;
+
+public abstract class GateServer {
+	abstract void startGateServer();
+}
