e224cc87d5f37e4e4f2acb85130a3b1c14ed0005
rioze <rioze@WIN-KD90QPEMRBI>
2017-07-31 22:57:13 +0800

    统一返回 object

--- ims/src/main/java/ims/webapp/sys/action/LoginAction.java
+++ ims/src/main/java/ims/webapp/sys/action/LoginAction.java
@@ -10,20 +10,17 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.util.WebUtils;
-import org.im.protocol.bean.RegisterBean;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.riozenc.quicktool.common.util.StringUtils;
-import com.riozenc.quicktool.common.util.xml.XmlUtils;
 
 import ims.common.Common;
 import ims.common.security.Principal;
 import ims.common.security.filter.PasswordShiroFilter;
 import ims.common.xml.XmlResultBean;
 import ims.webapp.BaseAction;
-import ims.webapp.acc.domain.UserDomain;
 
 @ControllerAdvice
 @RequestMapping("loginAction")
@@ -38,11 +35,7 @@ public class LoginAction extends BaseAction {
 	@ResponseBody
 	@RequestMapping(value = "login")
 	public Object login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-		
-		
-		
-//		httpServletRequest.setHeader("Content-type", "json;charset=UTF-8");
-		
+
 		String errorClassName = (String) httpServletRequest
 				.getAttribute(PasswordShiroFilter.DEFAULT_ERROR_KEY_ATTRIBUTE_NAME);
 		if (errorClassName == null) {
@@ -56,8 +49,7 @@ public class LoginAction extends BaseAction {
 			}
 
 			// 生成私钥 和 登录密钥
-return principal.getUser();
-//			return XmlUtils.object2xml(new XmlResultBean(Common.SUCCESS, XmlUtils.object2xml(principal.getUser())));
+			return principal.getUser();
 		} else {
 			// 失败
 			return loginFail(errorClassName, httpServletRequest, httpServletResponse);
@@ -74,7 +66,7 @@ return principal.getUser();
 		return null;
 	}
 
-	public String loginFail(String errorClassName, HttpServletRequest request, HttpServletResponse response) {
+	public Object loginFail(String errorClassName, HttpServletRequest request, HttpServletResponse response) {
 
 		String username = WebUtils.getCleanParam(request, PasswordShiroFilter.DEFAULT_USERNAME_PARAM);
 		boolean rememberMe = WebUtils.isTrue(request, PasswordShiroFilter.DEFAULT_REMEMBER_ME_PARAM);
@@ -91,28 +83,17 @@ return principal.getUser();
 			isValidateCodeLogin(username, true, false);
 			// model.addAttribute("isValidateCodeLogin", true);
 		}
-		
-		return XmlUtils.object2xml(new XmlResultBean(Common.FAILD, message));
+
+		return new XmlResultBean(Common.FAILD, message);
 	}
 
+	@ResponseBody
 	@RequestMapping(value = "/logout")
-	public String logout(String username, String password) {
+	public Object logout(String username, String password) {
 		Subject subject = SecurityUtils.getSubject();
 		subject.logout();
 		SecurityUtils.getSecurityManager().logout(subject);
-		return null;
-	}
-
-	/**
-	 * 包装通讯注册类
-	 * 
-	 * @param userDomain
-	 * @return
-	 */
-	private RegisterBean createRegister(UserDomain userDomain) {
-		RegisterBean registerBean = new RegisterBean();
-		registerBean.setUserId(userDomain.getUserId());
-		return registerBean;
+		return new XmlResultBean(Common.SUCCESS, "登出成功.");
 	}
 
 	public static boolean isValidateCodeLogin(String useruame, boolean isFail, boolean clean) {
